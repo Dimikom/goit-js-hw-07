9658cf877cee03b61f95a15cb6cb43e082e57685
Dmytro <dimikom@gmail.com>
2021-07-04 14:56:12 +0300

    Finish task 5

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -1,2 +1,9 @@
 const inputElem = document.querySelector("#name-input");
 const nameElem = document.querySelector("#name-output");
+
+function onInput() {
+  if (inputElem.value !== "") nameElem.textContent = inputElem.value;
+  else nameElem.textContent = "незнакомец";
+}
+
+inputElem.addEventListener(`input`, onInput);
